a0d030e07800aa0c556cfddbbfb5367b409b0ed9
nichoth <nichoth@gmail.com>
2021-10-23 14:46:53 -0700

    WIP better tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,11 @@ module.exports = {
   verbose: true,
   testEnvironment: "jsdom",
   testRegex: "(/__test__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  preset: 'ts-jest',
+  transformIgnorePatterns: ['node_modules/(?!(webnative|keystore-idb)/)'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
+  },
   moduleFileExtensions: [
     'js',
     'ts',
