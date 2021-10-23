52c6de13eeba857b0f63b9ee3b37aaf2c885d6ae
nichoth <nichoth@gmail.com>
2021-10-23 13:04:14 -0700

    setup test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,20 @@
 import wnBlob from '../src'
+import fs from 'fs'
+
+// this is a node buffer
+var buf = fs.readFileSync(__dirname + '/caracal.jpg')
+let arraybuffer = Uint8Array.from(buf).buffer;
+
+const blob = new Blob([arraybuffer as BlobPart], {
+    type: 'image/jpeg',
+  });
+
+var file = new File([blob], "caracal.jpg", { type: 'image/jpeg' });
+
+console.log('file', file)
 
 describe('example', () => {
-  it('should do something', () => {
-    expect(wnBlob('test')).toEqual('test')
-  })
+    it('should do something', () => {
+        expect(wnBlob('test')).toEqual('test')
+    })
 })
