3820f3a5e2482bea78f25f1aa2c12cf03ac41584
nichoth <nichoth@gmail.com>
2021-10-29 00:10:15 -0700

    it works

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@
 
 import wnBlob from '../dist'
 // import fs from 'fs'
-var fs = require('fs')
+// var fs = require('fs')
 import * as webnative from 'webnative/lib'
 var test = require('tape')
 
@@ -25,19 +25,20 @@ const PERMISSIONS = {
 var wn
 var file
 test('setup', t => {
-    console.log('fs', fs)
+    // console.log('fs', fs)
     // this is a node buffer
-    var buf = fs.readFileSync(__dirname + '/caracal.jpg')
-    let arraybuffer = Uint8Array.from(buf).buffer;
+    // var buf = fs.readFileSync(__dirname + '/caracal.jpg')
+    // let arraybuffer = Uint8Array.from(buf).buffer;
 
-    const blob = new Blob([arraybuffer], {
+    const blob = new Blob(['ok'], {
         type: 'image/jpeg',
     })
 
-    file = new File([blob], "caracal.jpg", { type: 'image/jpeg' });
+    file = new File([blob], "ok.jpg", { type: 'image/jpeg' });
 
     webnative.initialise({ permissions: PERMISSIONS })
         .then(_wn => {
+            console.log('_wnn', _wn)
             wn = _wn
             t.end()
         })
@@ -45,5 +46,9 @@ test('setup', t => {
 
 test('save a blob', t => {
     console.log('file.name', file.name)
+    const { fs } = wn
+    console.log('wn', wn)
+    var aaa = fs.write(fs.appPath(wn.path.file(file.name)), file)
+    console.log('aaa', aaa)
     t.end()
 })
